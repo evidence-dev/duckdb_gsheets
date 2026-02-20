1a966167f23779c9a253a96c4a12f24ffa77f0f2
Michael Harris <mharris@definite.app>
2026-02-20 14:38:38 -0700

    fix(secrets): inline secret access

--- src/include/utils/secret.hpp
+++ src/include/utils/secret.hpp
@@ -1,14 +1,12 @@
 #pragma once
 
 #include "duckdb/main/client_context.hpp"
-#include "duckdb/main/secret/secret.hpp"
 #include "duckdb/main/secret/secret_manager.hpp"
 
 namespace duckdb {
 namespace sheets {
 
 const SecretMatch GetSecretMatch(ClientContext &ctx, const std::string &path, const std::string &type);
-const KeyValueSecret *GetGSheetSecret(ClientContext &ctx);
 
 } // namespace sheets
 } // namespace duckdb
--- src/sheets/auth_factory.cpp
+++ src/sheets/auth_factory.cpp
@@ -8,28 +8,29 @@ namespace duckdb {
 namespace sheets {
 
 std::unique_ptr<IAuthProvider> CreateAuthFromSecret(ClientContext &ctx, IHttpClient &http) {
-	auto *secret = GetGSheetSecret(ctx);
-	if (!secret) {
-		return nullptr;
+	auto match = GetSecretMatch(ctx, "gsheet", "gsheet");
+	if (match.HasMatch()) {
+		auto &secret = match.GetSecret();
+		auto gsheet_secret = dynamic_cast<const KeyValueSecret *>(&secret);
+		auto provider = gsheet_secret->GetProvider();
+		if (provider == "key_file") {
+			Value emailValue, keyValue;
+			if (!gsheet_secret->TryGetValue("email", emailValue)) {
+				throw InvalidInputException("'email' not found in gsheet secret");
+			}
+			if (!gsheet_secret->TryGetValue("secret", keyValue)) {
+				throw InvalidInputException("'secret' not found in gsheet secret");
+			}
+			return make_uniq<ServiceAccountAuth>(http, emailValue.ToString(), keyValue.ToString());
+		} else {
+			Value tokenValue;
+			if (!gsheet_secret->TryGetValue("token", tokenValue)) {
+				throw InvalidInputException("'token' not found in gsheet secret");
+			}
+			return make_uniq<BearerTokenAuth>(tokenValue.ToString());
+		}
 	}
-
-	auto provider = secret->GetProvider();
-	if (provider == "key_file") {
-		Value emailValue, keyValue;
-		if (!secret->TryGetValue("email", emailValue)) {
-			throw InvalidInputException("'email' not found in gsheet secret");
-		}
-		if (!secret->TryGetValue("secret", keyValue)) {
-			throw InvalidInputException("'secret' not found in gsheet secret");
-		}
-		return make_uniq<ServiceAccountAuth>(http, emailValue.ToString(), keyValue.ToString());
-	} else {
-		Value tokenValue;
-		if (!secret->TryGetValue("token", tokenValue)) {
-			throw InvalidInputException("'token' not found in gsheet secret");
-		}
-		return make_uniq<BearerTokenAuth>(tokenValue.ToString());
-	}
+	return nullptr;
 }
 
 } // namespace sheets
